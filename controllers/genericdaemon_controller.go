bbd9c9b2ed484f2910294c13503308e9cce521e6
vijay-dcrust <vijay.pal@Vijays-MacBook-Pro.local>
2020-01-08 12:04:47 +0800

    Fixed the daemonset create error by inducing timewait

--- controllers/genericdaemon_controller.go
+++ controllers/genericdaemon_controller.go
@@ -17,19 +17,21 @@ package controllers
 
 import (
 	"context"
+	"fmt"
 	"github.com/go-logr/logr"
 	corev1 "k8s.io/api/core/v1"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/apimachinery/pkg/runtime"
-	"log"
 	"reflect"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/client"
+	"time"
 
 	batchv1 "example/api/v1"
 	appsv1 "k8s.io/api/apps/v1"
 	errors "k8s.io/apimachinery/pkg/api/errors"
-	types "k8s.io/apimachinery/pkg/types"
+	//types "k8s.io/apimachinery/pkg/types"
+	//"sigs.k8s.io/controller-runtime/pkg/controller/controllerutil"
 )
 
 // GenericDaemonReconciler reconciles a GenericDaemon object
@@ -44,13 +46,19 @@ type GenericDaemonReconciler struct {
 
 func (r *GenericDaemonReconciler) Reconcile(req ctrl.Request) (ctrl.Result, error) {
 	ctx := context.Background()
+	log := r.Log.WithValues("GenericDaemon", req.NamespacedName)
 
 	// your logic here
 	//var gd batchv1.GenericDaemon
 	gd := &batchv1.GenericDaemon{}
-	if err := r.Get(ctx, req.NamespacedName, gd); err != nil {
+	err := r.Get(ctx, req.NamespacedName, gd)
+	if err != nil && errors.IsNotFound(err) {
+		log.Info("Generic daemon not found. Deleted ! \n")
+		return ctrl.Result{}, nil
+	} else if err != nil {
 		return ctrl.Result{}, err
 	}
+	//fmt.Println("gd.Name:", gd.Name, "gd.NameSpace:", gd.Namespace)
 	// Define the desired Daemonset object
 	daemonset := &appsv1.DaemonSet{
 		ObjectMeta: metav1.ObjectMeta{
@@ -81,30 +89,37 @@ func (r *GenericDaemonReconciler) Reconcile(req ctrl.Request) (ctrl.Result, erro
 		return ctrl.Result{}, err
 	}
 	found := &appsv1.DaemonSet{}
-	err := r.Get(context.TODO(), types.NamespacedName{Name: daemonset.Name, Namespace: daemonset.Namespace}, found)
+	//err := r.Get(ctx, types.NamespacedName{Name: daemonset.Name, Namespace: daemonset.Namespace}, found)
+	err = r.Get(ctx, client.ObjectKey{Namespace: daemonset.Namespace, Name: daemonset.Name}, found)
 	if err != nil && errors.IsNotFound(err) {
-		log.Printf("Creating Daemonset %s/%s\n", daemonset.Namespace, daemonset.Name)
-		err := r.Create(context.TODO(), daemonset)
+		log.Info("Creating Daemonset \n", daemonset.Namespace, daemonset.Name)
+		err := r.Create(ctx, daemonset)
 		if err != nil {
 			return ctrl.Result{}, err
 		}
 	} else if err != nil {
 		return ctrl.Result{}, err
 	}
+	//Wait for the daemonset to come up
+	time.Sleep(2)
+	err = r.Get(ctx, client.ObjectKey{Namespace: daemonset.Namespace, Name: daemonset.Name}, found)
 	// Get the number of Ready daemonsets and set the Count status
-	if found.Status.NumberReady != gd.Status.Count {
-		log.Printf("Updating Status %s/%s\n", gd.Namespace, gd.Name)
-		gd.Status.Count = found.Status.NumberReady
-		err = r.Update(context.TODO(), gd)
+	if err == nil && found.Status.CurrentNumberScheduled != gd.Status.Count {
+		log.Info("Updating Status \n", gd.Namespace, gd.Name)
+		gd.Status.Count = found.Status.CurrentNumberScheduled
+		err = r.Update(ctx, gd)
 		if err != nil {
 			return ctrl.Result{}, err
 		}
 	}
+	fmt.Println("found.Name:", found.Name)
 	// Update the found object and write the result back if there are any changes
 	if !reflect.DeepEqual(daemonset.Spec, found.Spec) {
 		found.Spec = daemonset.Spec
-		log.Printf("Updating Daemonset %s/%s\n", daemonset.Namespace, daemonset.Name)
-		err = r.Update(context.TODO(), found)
+		log.Info("Updating Daemonset ", daemonset.Namespace, daemonset.Name)
+		fmt.Println("found.Name:", found.Name)
+		//fmt.Println("found.Spec.Label:", found.Spec.Template.Spec.Containers.)
+		err = r.Update(ctx, found)
 		if err != nil {
 			return ctrl.Result{}, err
 		}
